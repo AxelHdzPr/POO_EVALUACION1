41470967e4eb396182bc04ffafc3a7cde09fc4fc
AxelHdzPr <124701700+AxelHdzPr@users.noreply.github.com>
2023-02-10 18:19:09 -0600

    Add files via upload

---
+++ Proyecto1-Eva1-HolaMundo/src/proyecto1/eva1/holamundo/Proyecto1Eva1HolaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1.eva1.holamundo;
+
+/**
+ *
+ * @author priet
+ */
+public class Proyecto1Eva1HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
