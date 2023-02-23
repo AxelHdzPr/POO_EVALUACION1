d692335b968a69663fe5dd9675d669729ce629ae
AxelHdzPr <124701700+AxelHdzPr@users.noreply.github.com>
2023-02-23 09:40:05 -0600

    Add files via upload

---
+++ Eva1_13_Alcance/src/eva1_13_alcance/Eva1_13_Alcance.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_13_alcance;
+
+/**
+ *
+ * @author priet
+ */
+public class Eva1_13_Alcance {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 100;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("x = " + x);
+            System.out.println("i = " + i); 
+            System.out.println("j = " + j);
+            int j = 100;
+            System.out.println("j = " + j);
+            {
+        int z = 1000;
+        System.out.println("z = " + z);
+    }
+            System.out.println("z = " + z);
+        }//Termina bloque for
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+    }
+    public static void algo(){
+        System.out.println("x = " + x);
+}
+}
